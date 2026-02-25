14d1916d7b1524ac229c4750e0ad3c2698266043
zeeesea <zeuspositiv@gmail.com>
2026-02-26 00:24:21 +0100

    Improve GUI texture editor: unify sprite and direct texture updates, add atlas sprite detection utility

--- src/client/java/com/zeeesea/textureeditor/screen/GuiTextureEditorScreen.java
+++ src/client/java/com/zeeesea/textureeditor/screen/GuiTextureEditorScreen.java
@@ -88,21 +88,61 @@ public class GuiTextureEditorScreen extends AbstractEditorScreen {
         MinecraftClient client = MinecraftClient.getInstance();
         TextureManager.getInstance().putTexture(fullTextureId, canvas.getPixels(), canvas.getWidth(), canvas.getHeight());
 
-        if (isSpriteTexture && spriteAtlasId != null) {
-            // Sprite atlas based GUI textures
-            client.execute(() ->
-                    TextureManager.getInstance().applyLive(spriteAtlasId, canvas.getPixels(), canvas.getWidth(), canvas.getHeight()));
-        } else {
-            // Direct texture replacement (container UIs, etc.)
-            client.execute(() -> {
-                NativeImage img = new NativeImage(canvas.getWidth(), canvas.getHeight(), false);
-                for (int x = 0; x < canvas.getWidth(); x++)
-                    for (int y = 0; y < canvas.getHeight(); y++)
-                        img.setColorArgb(x, y, canvas.getPixels()[x][y]);
-                var dynamicTex = new net.minecraft.client.texture.NativeImageBackedTexture(img);
+        client.execute(() -> {
+            NativeImage img = new NativeImage(canvas.getWidth(), canvas.getHeight(), false);
+            for (int x = 0; x < canvas.getWidth(); x++) {
+                for (int y = 0; y < canvas.getHeight(); y++) {
+                    img.setColorArgb(x, y, canvas.getPixels()[x][y]);
+                }
+            }
+
+            Identifier spriteId = guiTextureId;
+            var atlasAndSprite = findSpriteInAtlases(client, spriteId);
+
+            if (atlasAndSprite != null) {
+                net.minecraft.client.texture.SpriteAtlasTexture atlas = atlasAndSprite.getLeft();
+                net.minecraft.client.texture.Sprite sprite = atlasAndSprite.getRight();
+                System.out.println("[TextureEditor] Updating sprite in atlas: " + spriteId);
+                atlas.bindTexture();
+                img.upload(0, sprite.getX(), sprite.getY(), false);
+            } else {
+                net.minecraft.client.texture.NativeImageBackedTexture dynamicTex = new net.minecraft.client.texture.NativeImageBackedTexture(img);
                 client.getTextureManager().registerTexture(fullTextureId, dynamicTex);
-            });
+                dynamicTex.bindTexture();
+                img.upload(0, 0, 0, false);
+                System.out.println("[TextureEditor] Applied live GUI texture: " + fullTextureId);
+            }
+        });
+    }
+
+    // Utility: Find sprite in block/gui atlases
+    private org.apache.commons.lang3.tuple.Pair<net.minecraft.client.texture.SpriteAtlasTexture, net.minecraft.client.texture.Sprite> findSpriteInAtlases(MinecraftClient client, Identifier id) {
+        var blockAtlas = client.getBakedModelManager().getAtlas(net.minecraft.client.texture.SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE);
+        var sprite = blockAtlas.getSprite(id);
+        if (sprite != null && !sprite.getContents().getId().getPath().equals("missingno")) {
+            if (sprite.getContents().getId().equals(id)) {
+                return org.apache.commons.lang3.tuple.Pair.of(blockAtlas, sprite);
+            }
         }
+        Identifier guiAtlasId = Identifier.of("textures/atlas/gui.png");
+        var tex = client.getTextureManager().getTexture(guiAtlasId);
+        if (tex instanceof net.minecraft.client.texture.SpriteAtlasTexture guiAtlas) {
+            sprite = guiAtlas.getSprite(id);
+            if (sprite != null && !sprite.getContents().getId().getPath().equals("missingno")) {
+                if (sprite.getContents().getId().equals(id)) {
+                    return org.apache.commons.lang3.tuple.Pair.of(guiAtlas, sprite);
+                }
+            }
+            if (id.getPath().startsWith("gui/sprites/")) {
+                String shortPath = id.getPath().substring("gui/sprites/".length());
+                Identifier shortId = Identifier.of(id.getNamespace(), shortPath);
+                sprite = guiAtlas.getSprite(shortId);
+                if (sprite != null && !sprite.getContents().getId().getPath().equals("missingno")) {
+                    return org.apache.commons.lang3.tuple.Pair.of(guiAtlas, sprite);
+                }
+            }
+        }
+        return null;
     }
 
     @Override
