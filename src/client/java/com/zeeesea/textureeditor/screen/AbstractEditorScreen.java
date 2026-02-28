85c24bb562ca17c6bfd1914bb94aaea37ca353ac
zeeesea <zeuspositiv@gmail.com>
2026-02-28 14:57:40 +0100

    removed changes

--- src/client/java/com/zeeesea/textureeditor/screen/AbstractEditorScreen.java
+++ src/client/java/com/zeeesea/textureeditor/screen/AbstractEditorScreen.java
@@ -30,8 +30,6 @@ public abstract class AbstractEditorScreen extends Screen {
     protected PixelCanvas canvas;
     protected int[][] originalPixels;
 
-    private int testfabric1214 = 1;
-
     // Tint support (used by block editors)
     protected int blockTint = -1;
     protected boolean isTinted = false;
